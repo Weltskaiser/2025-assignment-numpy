045e9b5d697021bd1ebc1b30362d42a53e30ab73
Nicolas LENGLET <nicolas.lenglet35@gmail.com>
2025-12-15 14:42:25 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,10 +37,18 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
 
     # TODO
+
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Not a numpy array")
+
+    if not len(X.shape) == 2:
+        raise ValueError("Numpy array has a shape different from 2")
+
+    indexes = np.unravel_index(X.argmax(), X.shape)
+    i = int(indexes[0])
+    j = int(indexes[1])
 
     return i, j
 
@@ -64,4 +72,14 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    prod = 1
+    for n in range(1, n_terms + 1):
+        prod *= (4*n**2) / (4*n**2 - 1)
+
+    return prod * 2
+
+
+# X = np.array([[0, 1], [2, 0]])
+# print(max_index(X))
+# print(wallis_product(2))
